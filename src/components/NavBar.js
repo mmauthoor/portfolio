795d72bcb0bea5d7cd47f72c5acc23f90897c907
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-24 00:52:19 +1100

    Add icons for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,46 @@
 import { Link } from 'react-router-dom';
+import {
+  FaBriefcase,
+  FaCode,
+  FaPhoneAlt,
+  FaReact,
+  FaRegAddressCard,
+  FaUniversity
+} from 'react-icons/fa';
+
 import './NavBar.css';
 
 export default function NavBar() {
   return (
     <nav>
-      <Link to='/' className='current'>Profile</Link>
-      <Link to='/projects'>Projects</Link>
-      <Link to='/experience'>Experience</Link>
-      <Link to='/education'>Education</Link>
+      <Link to='/' className='current'>
+        <FaRegAddressCard className='icon' />
+        Profile
+      </Link>
+
+      <Link to='/projects'>
+        <FaCode className='icon' />
+        Projects
+      </Link>
+
+      <Link to='/experience'>
+        <FaBriefcase className='icon' />
+        Experience
+      </Link>
+
+      <Link to='/education'>
+        <FaUniversity className='icon' />
+        Education
+      </Link>
+
+      <Link to='/contact'>
+        <FaPhoneAlt className='icon' />
+        Contact
+      </Link>
+
       <div className='divider' />
-      <Link to='/contact'>Contact</Link>
+
+      <p>This site was built with <FaReact alt='React' /></p>
     </nav>
   );
 }

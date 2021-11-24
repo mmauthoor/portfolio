a1265942af25fcda0c84314f1c2d3a93abacaba7
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-24 20:25:15 +1100

    Create separate component for routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-
 import './App.css';
 import NavBar from './components/NavBar';
-import Profile from './views/Profile';
-import Projects from './views/Projects';
-import Experience from './views/Experience';
-import Education from './views/Education';
-import Contact from './views/Contact';
+import Router from './routes/Router';
 
 function App() {
   return (
     <div className="App">
-
       <NavBar />
-
-      <Routes>
-        <Route path='/' element={<Profile />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/experience' element={<Experience />} />
-        <Route path='/education' element={<Education />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-
+      <Router />
     </div>
   );
 }
---
+++ src/routes/Router.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Profile from '../views/Profile';
+import Projects from '../views/Projects';
+import Experience from '../views/Experience';
+import Education from '../views/Education';
+import Contact from '../views/Contact';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<Profile />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/experience' element={<Experience />} />
+      <Route path='/education' element={<Education />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+  );
+}

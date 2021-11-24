7dae5f85807c59697098e5b73a63c83ffbaa0304
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-24 21:22:50 +1100

    Debug deployment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Router from './routes/Router';
 
 function App() {
+  console.log(process.env.PUBLIC_URL);
   return (
     <div className="App">
       <NavBar />
